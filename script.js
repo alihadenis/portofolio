d9ec68c2b63e922abdc99a339e06ffb36d60ea48
alihadens <denisalishaputra@gmail.com>
2024-10-15 20:52:47 +0700

    menambahkan landing website portofolio

---
+++ script.js
@@ -0,0 +1,10 @@
+const bar = document.querySelector('.bar');
+const lii = document.querySelectorAll('.nav-link');
+
+// Ketika 'bar' diklik, toggle 'active' untuk semua elemen dengan class 'nav-link'
+bar.addEventListener('click', () => {
+  lii.forEach((link) => {
+    link.classList.toggle('active');
+    link.classList.toggle('nav-link');
+  });
+});

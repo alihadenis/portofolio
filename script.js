336ca7fdad00092e12ef6e1547b5cf5a29b950b3
alihadens <denisalishaputra@gmail.com>
2024-10-18 21:52:17 +0700

    mengubah class bar lengsung ke class toggle agar terkena semua barnya,

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bar = document.querySelector('.bar');
+const bar = document.querySelector('.toggle-container');
 const lii = document.querySelectorAll('.nav-link');
 
 // Ketika 'bar' diklik, toggle 'active' untuk semua elemen dengan class 'nav-link'
